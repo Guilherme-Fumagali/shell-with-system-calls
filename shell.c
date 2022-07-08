94ef9b7d203e9517d0229744d27d0523690c765b
guilherme-fumagali <guilhermefumagali@estudante.ufscar.br>
2022-07-07 21:03:01 -0300

    Implementado a entrada de argumentos

--- shell.c
+++ shell.c
@@ -5,20 +5,24 @@
 #include <sys/wait.h>
 
 #define MAX 256
+#define MAX_ARGS 5
 
 int main() {
   char comando[MAX];
-  char* args[5];
+  char* args[1 + MAX_ARGS] = {};
   int pid;
 
   while (1) {
+    for (unsigned i = 0; i < MAX_ARGS + 1; i++)
+      args[i] = '\0';
+
     printf("> ");
     fgets(comando, MAX, stdin);
-    
-    char *str = strtok(comando, " ");
-    for (unsigned i = 0; str != NULL; str = strtok(NULL, " "), i ++){
-      args[i] = str;
-    }
+    comando[strlen(comando) - 1] = '\0';
+
+    char* str = strtok(comando, " ");
+    for (unsigned i = 0; str != NULL && i < MAX_ARGS; str = strtok(NULL, " "), i ++)
+        args[i] = str;
 
     if (!strcmp(comando, "exit")) {
       exit(EXIT_SUCCESS);
@@ -28,7 +32,7 @@ int main() {
     if (pid) {
       waitpid(pid, NULL, 0);
     } else {
-      execvp(comando, args);
+      execvp(args[0], &args[0]);
       printf("Erro ao executar comando!\n");
       exit(EXIT_FAILURE);
     }
