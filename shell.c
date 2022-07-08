26519b69e9ae5969eb19fd3fea0b4de0acd8f237
guilherme-fumagali <guilhermefumagali@estudante.ufscar.br>
2022-07-07 21:27:40 -0300

    Adicionado suporte a comandos em segundo plano

--- shell.c
+++ shell.c
@@ -6,31 +6,40 @@
 
 #define MAX 256
 #define MAX_ARGS 5
+typedef enum {false, true} bool;
 
 int main() {
   char comando[MAX];
-  char* args[1 + MAX_ARGS] = {};
+  char* args[MAX_ARGS + 1];
   int pid;
 
   while (1) {
-    for (unsigned i = 0; i < MAX_ARGS + 1; i++)
-      args[i] = '\0';
+    for (unsigned j = 0; j < MAX_ARGS + 1; j++)
+      args[j] = '\0';
 
     printf("> ");
     fgets(comando, MAX, stdin);
     comando[strlen(comando) - 1] = '\0';
 
     char* str = strtok(comando, " ");
-    for (unsigned i = 0; str != NULL && i < MAX_ARGS; str = strtok(NULL, " "), i ++)
+    unsigned i;
+    for (i = 0; str != NULL && i < MAX_ARGS; str = strtok(NULL, " "), i ++)
         args[i] = str;
 
     if (!strcmp(comando, "exit")) {
       exit(EXIT_SUCCESS);
     }
 
+    bool segundoPlano = false;
+    if(!strcmp(args[i - 1], "&")){
+      args[i - 1] = '\0';  
+      segundoPlano = true;
+    }
+    
     pid = fork();
     if (pid) {
-      waitpid(pid, NULL, 0);
+      if(!segundoPlano)
+        waitpid(pid, NULL, 0);
     } else {
       execvp(args[0], &args[0]);
       printf("Erro ao executar comando!\n");
